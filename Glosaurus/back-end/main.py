cdc731ca6c475e0ee897255effa7fef790e0b159
Ruben Durand <ruben.durand@etu.unilim.fr>
2026-01-26 14:53:20 +0100

    modification du tauri conf et ajout de spécificité par OS et modif du yaml pour fix et release arm64

--- Glosaurus/back-end/main.py
+++ Glosaurus/back-end/main.py
@@ -9,8 +9,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    allow_origins=["http://localhost:5173"]
+    allow_headers=["*"]
 
 )
 
